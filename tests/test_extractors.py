3c36028f821ff3a903666e5b4fde313db5b5ed7c
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-08-03 01:58:36 +0200

    add syncplay testing

--- tests/test_extractors.py
+++ tests/test_extractors.py
@@ -50,27 +50,36 @@ def clear_screen():
     else:
         _ = system('clear')
 
-
-def test_provider(data, provider_name, get_direct_link_func):
-    print(f"{provider_name} TEST")
+def test_provider(data, provider_name, get_direct_link_func, verbose=False) -> dict:
+    if verbose:
+        print(f"{provider_name} TEST")
+        
+    direct_links = {}
     for language in data[provider_name]:
         soup = BeautifulSoup(make_request(data[provider_name][language]), 'html.parser')
         direct_link = get_direct_link_func(soup)
-        language_str = str(language).replace("1", "German Dub").replace("2", "English Sub").replace("3", "German Sub")
-        #print(f"{language_str}: {direct_link}")
-        if 'http' in direct_link:
-            print("\033[92mOK\033[0m - Direct link found.")
-        else:
-            print("\033[91mFAILURE\033[0m - No valid direct link found.")
-    print()
+        direct_links[language] = direct_link
+        
+    if verbose:
+        for link in direct_links.values():
+            if 'http' in link:
+                print("\033[92mOK\033[0m - Direct link found.")
+            else:
+                print("\033[91mFAILURE\033[0m - No valid direct link found.")
+        print()
+        
+    return direct_links
 
+def main():
+    url = "https://aniworld.to/anime/stream/demon-slayer-kimetsu-no-yaiba/staffel-1/episode-1"
+    soup = BeautifulSoup(make_request(url), 'html.parser')
+    data = providers(soup)
 
-url = "https://aniworld.to/anime/stream/demon-slayer-kimetsu-no-yaiba/staffel-1/episode-1"
-soup = BeautifulSoup(make_request(url), 'html.parser')
-data = providers(soup)
+    clear_screen()
+    test_provider(data, "VOE", voe_get_direct_link, True)
+    test_provider(data, "Doodstream", doodstream_get_direct_link, True)
+    test_provider(data, "Vidoza", vidoza_get_direct_link, True)
+    test_provider(data, "Streamtape", streamtape_get_direct_link, True)
 
-clear_screen()
-test_provider(data, "VOE", voe_get_direct_link)
-test_provider(data, "Doodstream", doodstream_get_direct_link)
-test_provider(data, "Vidoza", vidoza_get_direct_link)
-test_provider(data, "Streamtape", streamtape_get_direct_link)
+if __name__ == "__main__":
+    main()
