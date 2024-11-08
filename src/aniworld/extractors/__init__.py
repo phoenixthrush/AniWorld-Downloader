df26f422f6e024ee5d77e274b112b1d0d0c73017
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-11-08 16:59:04 +0100

    add streamkistetv extractor

--- src/aniworld/extractors/__init__.py
+++ src/aniworld/extractors/__init__.py
@@ -2,3 +2,5 @@ from .doodstream import doodstream_get_direct_link
 from .streamtape import streamtape_get_direct_link
 from .vidoza import vidoza_get_direct_link
 from .voe import voe_get_direct_link
+
+from .streamkiste import streamkiste_get_direct_link
---
+++ src/aniworld/extractors/streamkiste.py
@@ -0,0 +1,126 @@
+"""
+MIT License
+
+Copyright (c) 2024 Phoenixthrush UwU
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import os
+import subprocess
+import re
+import requests
+
+from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
+from aniworld.extractors import voe_get_direct_link
+from aniworld import globals as aniworld_globals
+
+
+def clear_screen() -> None:
+        import platform
+        import os
+        if platform.system() == "Windows":
+            os.system("cls")
+        else:
+            os.system("clear")
+
+
+def fetch_direct_link(link):
+    filtered_urls = []
+
+    with sync_playwright() as p:
+        # TODO - add firefox or chromium fallback
+        browser = p.webkit.launch(headless=True)
+        page = browser.new_page()
+
+        page.on("request", lambda request: filtered_urls.append(request.url) if re.match(r"https://voe\.sx/e/.*", request.url) else None)
+
+        page.goto(link)
+
+        # TODO - check for google captcha
+
+        page.wait_for_selector("div.info-right")
+        title_element = page.query_selector("div.info-right .title h1")
+        title_text = title_element.inner_text().strip() if title_element else "StreamKisteTV"
+
+        page.wait_for_selector("div#single-stream div#stream li.stream div#stream-links a")
+        first_stream_link = page.query_selector("div#single-stream div#stream li.stream div#stream-links a")
+
+        if first_stream_link:
+            first_stream_link.click()
+
+        page.wait_for_timeout(5000)
+        browser.close()
+
+    try:
+        response = requests.get(filtered_urls[0], timeout=30, headers={'User-Agent': aniworld_globals.DEFAULT_USER_AGENT})
+    except IndexError:
+        # TODO
+        # add fallback using another provider or
+        # doing the request again with higher timeout and headless=False
+
+        print("No VOE redirect link found.")
+        return
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # TODO - add other provider options -> Streamtape
+    direct_link = voe_get_direct_link(soup)
+
+    return direct_link, title_text
+
+
+def download_video(link, title):
+    FILENAME = os.path.join("Downloads", f"{title}.mp4")
+    subprocess.run(['yt-dlp', '--quiet', '--no-warnings', '--progress', '-o', FILENAME, link])
+
+
+def watch_video(link, title):
+    mpv_title = title.replace(" ", "_")
+    subprocess.run(['mpv', f'--force-media-title={mpv_title}', '--fs', '--quiet', '--really-quiet', link])
+
+
+def streamkiste_get_direct_link(link: str):
+    return fetch_direct_link(link)
+
+
+def main():
+    try:
+        clear_screen()
+        link = input("Enter the StreamKisteTV link: ")
+        action = input("(D)ownload or (W)atch?: ").strip().lower()
+
+        try:
+            link, title = streamkiste_get_direct_link(link)
+        except TypeError:
+            return
+
+        if action == 'd':
+            download_video(link, title)
+        elif action == 'w':
+            watch_video(link, title)
+        else:
+            print("Invalid option. Please choose 'D' for download or 'W' for watch.")
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    main()
