535e526c84d5bd25556f428846509effd450e25d
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-11-11 21:42:43 +0100

    update extractor folder structure

--- src/aniworld/extractors/__init__.py
+++ src/aniworld/extractors/__init__.py
@@ -1,4 +1,7 @@
-from .aniworld.doodstream import doodstream_get_direct_link
-from .aniworld.streamtape import streamtape_get_direct_link
-from .aniworld.vidoza import vidoza_get_direct_link
-from .aniworld.voe import voe_get_direct_link
+from .provider.doodstream import doodstream_get_direct_link
+from .provider.streamtape import streamtape_get_direct_link
+from .provider.vidoza import vidoza_get_direct_link
+from .provider.voe import voe_get_direct_link
+
+from .nhentai.nhentai import main
+from .streamkiste.streamkiste import main
