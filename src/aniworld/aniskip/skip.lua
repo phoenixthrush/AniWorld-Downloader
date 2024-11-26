4e27086ff15b68a23c043122e03f6df92a68042b
Tmaster055 <gratzer.tobias3@gmail.com>
2024-11-26 21:32:44 +0100

    Fix mpv stuck at zero

--- src/aniworld/aniskip/skip.lua
+++ src/aniworld/aniskip/skip.lua
@@ -8,16 +8,13 @@ mpv_options.read_options(options, "skip")
 
 local function skip()
     local current_time = mp.get_property_number("time-pos")
+    if not current_time then return end
 
-    if not current_time then
-        return
-    end
-
-    if current_time >= options.op_start and current_time < (options.op_start + 1) then
+    if options.op_start ~= options.op_end and current_time >= options.op_start and current_time < (options.op_start + 1) then
         mp.set_property_number("time-pos", options.op_end)
     end
 
-    if current_time >= options.ed_start and current_time < (options.ed_start + 1) then
+    if options.ed_start ~= options.ed_end and current_time >= options.ed_start and current_time < (options.ed_start + 1) then
         mp.set_property_number("time-pos", options.ed_end)
     end
 end
