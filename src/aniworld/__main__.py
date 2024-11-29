6cd4543569bbe76afe340f0cc74589cc03d4255d
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-11-29 16:00:48 +0100

    add episode args in main

--- src/aniworld/__main__.py
+++ src/aniworld/__main__.py
@@ -1,4 +1,4 @@
-from aniworld.models import Anime, Episode
+from aniworld.models import Anime, Episode, get_episode_from_link, get_season_from_link
 from aniworld.search import search_anime
 from aniworld.execute import execute
 from aniworld.parser import parse_arguments
@@ -7,54 +7,34 @@ from aniworld.parser import parse_arguments
 def main() -> None:
     try:
         arguments = parse_arguments()
-        slug = search_anime()
+        anime_list = []
 
-        user_episode = Episode(
-            slug=slug,
-            season=1,
-            episode=1
-        )
-
-        user_episode_2 = Episode(
-            slug=slug,
-            season=1,
-            episode=2
-        )
-
-        kaguya_episode = Episode(
-            link="https://aniworld.to/anime/stream/kissxsis/staffel-1/episode-3"
-        )
-
-        kaguya_episode2 = Episode(
-            link="https://aniworld.to/anime/stream/kissxsis/staffel-1/episode-3"
-        )
-
-        kaguya_episode3 = Episode(
-            link="https://aniworld.to/anime/stream/kissxsis/staffel-1/episode-3"
-        )
-
-        alya_episode = Episode(
-            slug="alya-sometimes-hides-her-feelings-in-russian",
-            season=1,
-            episode=2
-        )
-
-        execute(
-            anime_list=[
-                Anime(
-                    **({"action": arguments.action} if arguments.action else {}),
-                    episode_list=[user_episode, user_episode_2]
-                ),
-                Anime(
-                    **({"action": arguments.action} if arguments.action else {}),
-                    episode_list=[kaguya_episode, kaguya_episode2, kaguya_episode3]
-                ),
-                Anime(
-                    **({"action": arguments.action} if arguments.action else {}),
-                    episode_list=[alya_episode]
+        if arguments.episodes:
+            for episode_link in arguments.episodes:
+                episode = Episode(
+                    slug=episode_link.split("/")[-3],
+                    season=get_season_from_link(link=episode_link),
+                    episode=get_episode_from_link(link=episode_link)
                 )
+                anime_list.append(Anime(
+                    **({"action": arguments.action} if arguments.action else {}),
+                    episode_list=[episode]
+                ))
+        else:
+            slug = search_anime()
+
+            default_episodes = [
+                Episode(slug=slug, season=1, episode=1),
+                Episode(slug=slug, season=1, episode=2),
+                Episode(slug=slug, season=1, episode=3)
             ]
-        )
+
+            anime_list.append(Anime(
+                **({"action": arguments.action} if arguments.action else {}),
+                episode_list=default_episodes
+            ))
+
+        execute(anime_list=anime_list)
 
     except KeyboardInterrupt:
         pass
