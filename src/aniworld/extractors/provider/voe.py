812f324be5164ace839b990a32fcc0d1cab9f26c
phoenixthrush <mr.cetto@phoenixthrush.com>
2024-11-29 20:06:57 +0100

    fix voe extractor

--- src/aniworld/extractors/provider/voe.py
+++ src/aniworld/extractors/provider/voe.py
@@ -2,36 +2,39 @@ from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
 import logging
 import re
+from base64 import b64decode
 
 from aniworld import globals as aniworld_globals
 
 REDIRECT_PATTERN = re.compile(r"window\.location\.href\s*=\s*'(https://[^/]+/e/\w+)';")
-NODE_DETAILS_PATTERN = re.compile(r'let nodeDetails = prompt\("Node",\s*"(https://[^"]+)"\);')
+EXTRACT_VEO_HLS_PATTERN = re.compile(r"'hls': '(?P<hls>.*)'")
 
 
 def voe_get_direct_link(soup):
     redirect_match = REDIRECT_PATTERN.search(str(soup.prettify))
-    if redirect_match:
-        redirect_url = redirect_match.group(1)
+    if not redirect_match:
+        logging.warning("No redirect link found.")
+        return None
 
-        try:
-            with urlopen(
-                Request(
-                    redirect_url,
-                    headers={'User-Agent': aniworld_globals.DEFAULT_USER_AGENT}
-                ),
-                timeout=10
-            ) as response:
-                redirect_content = response.read()
-            redirect_content_str = redirect_content.decode('utf-8')
-        except (HTTPError, URLError, TimeoutError) as e:
-            print(f"Failed to fetch URL {redirect_url}: {e}")
-            redirect_content_str = None
+    redirect_url = redirect_match.group(1)
+    try:
+        with urlopen(
+            Request(
+                redirect_url,
+                headers={'User-Agent': aniworld_globals.DEFAULT_USER_AGENT}
+            ),
+            timeout=10
+        ) as response:
+            redirect_content = response.read()
+        redirect_content_str = redirect_content.decode('utf-8')
+    except (HTTPError, URLError, TimeoutError) as e:
+        logging.error(f"Failed to fetch URL {redirect_url}: {e}")
+        return None
 
-        if redirect_content_str:
-            node_details_match = NODE_DETAILS_PATTERN.search(redirect_content_str)
-            if node_details_match:
-                return node_details_match.group(1)
+    hls_match = EXTRACT_VEO_HLS_PATTERN.search(redirect_content_str)
+    if not hls_match:
+        logging.warning("No HLS link found.")
+        return None
 
-    logging.warning("No direct link found.")
-    return None
+    hls_link = b64decode(hls_match.group("hls")).decode()
+    return hls_link
