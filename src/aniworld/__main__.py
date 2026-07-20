ceb0a2d51e16a51f8ad3184b3030c652a82db6f2
Mr. Cetto <contact@phoenixthrush.com>
2026-07-21 00:22:48 +0200

    maybe fix nuitka build

--- src/aniworld/__main__.py
+++ src/aniworld/__main__.py
@@ -6,6 +6,7 @@
 # nuitka-project: --static-libpython=no
 # nuitka-project: --assume-yes-for-downloads
 # nuitka-project: --python-flag=-m
+# nuitka-project: --mode=app
 
 # Include hidden imports (dynamically loaded modules that Nuitka can't detect)
 # nuitka-project: --include-package=urllib3.contrib
@@ -19,16 +20,14 @@
 # nuitka-project: --include-data-file=src/aniworld/aniskip/scripts/aniskip.lua=aniworld/aniskip/scripts/aniskip.lua
 # nuitka-project: --include-data-file=src/aniworld/aniskip/scripts/autoexit.lua=aniworld/aniskip/scripts/autoexit.lua
 # nuitka-project: --include-data-file=src/aniworld/aniskip/scripts/autostart.lua=aniworld/aniskip/scripts/autostart.lua
+# nuitka-project: --include-data-file=src/aniworld/browsers.jsonl=aniworld/browsers.jsonl
 
 # Platform-specific flags
 # nuitka-project-if: {OS} == "Darwin":
-#    nuitka-project: --standalone
-#    nuitka-project: --macos-create-app-bundle
 #    nuitka-project: --macos-app-name=AniWorld
 #    nuitka-project: --macos-app-icon=src/aniworld/nuitka/icon.webp
 
 # nuitka-project-if: {OS} in ("Windows", "Linux", "FreeBSD"):
-#    nuitka-project: --onefile
 #    nuitka-project: --windows-icon-from-ico=src/aniworld/nuitka/icon.webp
 
 # ========================
