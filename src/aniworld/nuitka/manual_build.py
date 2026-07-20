e2d951a0e876259890274e8ea2d9999f39262ed2
Yezun-hikari <Yezun-hikari@users.noreply.github.com>
2026-07-20 22:34:56 +0200

    Refactor: Replace fake_useragent with random list

--- src/aniworld/nuitka/manual_build.py
+++ src/aniworld/nuitka/manual_build.py
@@ -2,18 +2,12 @@ import subprocess
 
 
 def build():
-    # Include browsers.jsonl from fake_useragent package
-    # JSON_PATH = os.path.join(
-    #    os.path.dirname(fake_useragent.__file__), "data", "browsers.jsonl"
-    # )
-
     subprocess.run(
         [
             "python",
             "-m",
             "nuitka",
             "src/aniworld",
-            # "--include-data-file=" + JSON_PATH + "=fake_useragent/data/browsers.jsonl",
         ]
     )
 
